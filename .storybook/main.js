ba3249dc57a74fc00f4aff6d8442fc63c9c7049d
iyorozuya <iyaksha@tutanota.com>
2023-01-23 20:28:06 +0530

    feat: added tailwindcss support

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,14 @@ module.exports = {
         "../packages/**/src/stories/*.stories.mdx",
     ],
     addons: [
+        {
+            name: "@storybook/addon-postcss",
+            options: {
+                postcssLoaderOptions: {
+                    implementation: require("postcss"),
+                },
+            },
+        },
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
--- packages/components/src/Button.tsx
+++ packages/components/src/Button.tsx
@@ -1,5 +1,5 @@
 export const Button = (): React.ReactElement => {
     return (
-        <button>Click me</button>
+        <button className="bg-blue-500 text-white rounded-md shadow-lg px-4 py-2">Click me</button>
     )
 }
